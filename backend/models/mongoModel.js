71da47f8d0463a3880bc6f858c2838ff23cc0792
DikshitRJ <ourledtv@gmail.com>
2023-06-07 16:54:39 +0530

    Initial commit
    This comment consists the workspace prepared, with empty files and the mongoose model

---
+++ backend/models/mongoModel.js
@@ -0,0 +1,27 @@
+const mongo=require('mongodb')
+const mongoose=require('mongoose')
+const express=require('express')
+const schema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"name not entered"]
+    },
+    email:{
+        type:String,
+        required:[true,"email not entered"]
+    },
+    phno:{
+        type:String,
+        required:[true,"phone not entered"]
+    },
+    pswd:{
+        type:String,
+        required:[true,"password not entered"]
+    },
+    taskArray:{
+        type:Array,
+        required:true
+    }
+},({collection:accounts}))
+const model=new mongoose.model(schema,"accountsModel")
+module.exports=model
