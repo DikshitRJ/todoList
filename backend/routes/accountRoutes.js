44d1af7da51b2916f4b49a81f4da4aefdb371dd3
DikshitRJ <ourledtv@gmail.com>
2023-06-11 17:29:54 +0530

    Added nodemailer support
    In this commit, I have added support for sending verification emails using nodemailer with the MongoDB ID

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -5,6 +5,8 @@ const mongodb = new MongoClient("mongodb://localhost:27017/");
 const bodyparser=require('body-parser');
 const accountsModel=require('../models/accountsModel');
 const unverifiedModel=require('../models/unverifiedModel');
+const nodemailer=require('nodemailer');
+const mailer=require('./mailer');
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/todoList');
 }
@@ -19,6 +21,13 @@ router.post('/new',async(req, res)=>{
     await newAccount.validate();
     await newAccount.save();
     const doc=await unverified.findOne({name:newAccount.name});
-    console.log(doc._id.toString());
-})
+    let info = await mailer.transporter.sendMail({
+        from: '"DikshitRJ" <ourledtv@gmail.com>', // sender address
+        to: "ourledtv@gmail.com", // list of receivers
+        subject: "Verification link ✔", // Subject line
+        text: `GoTo: http://${process.env.URL}/account/verify?id=${doc._id.toString()}`, // plain text body
+        html: `<b>GoTo: http://${process.env.URL}/account/verify?id=${doc._id.toString()}</b>`, // html body
+    });
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+});
 module.exports = router;
